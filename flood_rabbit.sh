2c0aefdf9f9283a5cae9e6cafef3ec01b4a4989e
Joel Carlbark <joel.carlbark@findwise.com>
2013-11-11 10:26:30 +0100

    Change publish script to have the uri as a variable

--- flood_rabbit.sh
+++ flood_rabbit.sh
@@ -1,5 +1,6 @@
+exchange_uri="http://guest:guest@localhost:15672/api/exchanges/%2f/test/publish"
 count=1
 while true; do
-curl --verbose --request POST -d "{\"properties\":{}, \"routing_key\":\"mykey\", \"payload\":\"mybody $count\", \"payload_encoding\":\"string\"}" http://guest:guest@localhost:15672/api/exchanges/%2f/test/publish > /dev/null 2>&1
+curl --verbose --request POST -d "{\"properties\":{}, \"routing_key\":\"mykey\", \"payload\":\"mybody $count\", \"payload_encoding\":\"string\"}" $exchange_uri > /dev/null 2>&1
 count=$[$count+1]
 done
